4cca2aaeaf3ba18d179b55ac51a9478b1a8f04f0
martinkors <martinkors@live.dk>
2021-02-11 19:53:24 +0100

    Commented out some boilerplate code

--- server/src/server.ts
+++ server/src/server.ts
@@ -124,37 +124,37 @@ async function validateTextDocument(textDocument: TextDocument): Promise<void> {
 
 	let problems = 0;
 	let diagnostics: Diagnostic[] = [];
-	while ((m = pattern.exec(text)) && problems < settings.maxNumberOfProblems) {
-		problems++;
-		let diagnostic: Diagnostic = {
-			severity: DiagnosticSeverity.Warning,
-			range: {
-				start: textDocument.positionAt(m.index),
-				end: textDocument.positionAt(m.index + m[0].length)
-			},
-			message: `${m[0]} is all uppercase.`,
-			source: 'ex'
-		};
-		if (hasDiagnosticRelatedInformationCapability) {
-			diagnostic.relatedInformation = [
-				// {
-				// 	location: {
-				// 		uri: textDocument.uri,
-				// 		range: Object.assign({}, diagnostic.range)
-				// 	},
-				// 	message: 'Spelling matters'
-				// },
-				// {
-				// 	location: {
-				// 		uri: textDocument.uri,
-				// 		range: Object.assign({}, diagnostic.range)
-				// 	},
-				// 	message: 'Particularly for names'
-				// }
-			];
-		}
-		diagnostics.push(diagnostic);
-	}
+	// while ((m = pattern.exec(text)) && problems < settings.maxNumberOfProblems) {
+	// 	problems++;
+	// 	let diagnostic: Diagnostic = {
+	// 		severity: DiagnosticSeverity.Warning,
+	// 		range: {
+	// 			start: textDocument.positionAt(m.index),
+	// 			end: textDocument.positionAt(m.index + m[0].length)
+	// 		},
+	// 		message: `${m[0]} is all uppercase.`,
+	// 		source: 'ex'
+	// 	};
+	// 	if (hasDiagnosticRelatedInformationCapability) {
+	// 		diagnostic.relatedInformation = [
+	// 			// {
+	// 			// 	location: {
+	// 			// 		uri: textDocument.uri,
+	// 			// 		range: Object.assign({}, diagnostic.range)
+	// 			// 	},
+	// 			// 	message: 'Spelling matters'
+	// 			// },
+	// 			// {
+	// 			// 	location: {
+	// 			// 		uri: textDocument.uri,
+	// 			// 		range: Object.assign({}, diagnostic.range)
+	// 			// 	},
+	// 			// 	message: 'Particularly for names'
+	// 			// }
+	// 		];
+	// 	}
+	// 	diagnostics.push(diagnostic);
+	// }
 
 	connection.sendDiagnostics({ uri: textDocument.uri, diagnostics });
 }
